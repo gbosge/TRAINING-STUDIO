04265b4d1f25ed8c74e3ed666bd35ff63ebe98da
Gbenga Ogunjobi <gbenga_ogunjobi@yahoo.com>
2023-05-30 12:41:27 +0100

    Initial commit

---
+++ element.js
@@ -0,0 +1,19 @@
+// function changeColor(newColor){
+//     const elem=document.getElementById('parach'); 
+//     elem.style.color = newColor
+// }
+// function ronaldo(fontSize){
+//     var nani=document.getElementById('game'); 
+//     nani.style.fontSize = fontSize
+// }
+
+function fontSize(increaseFont){
+    var run=document.getElementById ('sade');
+    run.style.fontSize = increaseFont
+}
+
+function font(changeFont){
+    var read=document.getElementById ('sola');
+    read.style.fontFamily = changeFont
+}
+
